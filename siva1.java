0ca2a5f92c929ec6c5f52494bfe024f897893a75
madhu <madhukadiyam84@gmail.com>
2020-08-15 10:38:18 -0700

    git pushing theory

--- siva1.java
+++ siva1.java
@@ -1,2 +1,9 @@
-this is parameswara
-this is edited
+git init
+mkdir parameswara
+cd parameswara
+vi siva1.java
+git add siva1.java
+git commit -m "siva1.java file pushed"
+git remote add origin "github new repository url paste here"
+git remote add origin URL
+git push -u origin master
